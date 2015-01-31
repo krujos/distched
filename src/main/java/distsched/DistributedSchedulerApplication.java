3019380e99d536b6c7ebd6a576e435aa376d4774
krujos <jkruck@pivotal.io>
2015-01-31 09:46:31 -0600

    ticking and tocking

--- src/main/java/distsched/DistributedSchedulerApplication.java
+++ src/main/java/distsched/DistributedSchedulerApplication.java
@@ -1,12 +1,33 @@
 package distsched;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
+@EnableScheduling
 public class DistributedSchedulerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DistributedSchedulerApplication.class, args);
-    }
+	@Scheduled(fixedDelay = 2000)
+	private void tick() {
+		System.out.println("tick");
+	}
+
+	@Scheduled(cron = "* * * * * *")
+	private void tock() {
+		System.out.println("tock");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedSchedulerApplication.class, args);
+		while (true) {
+		}
+	}
 }
