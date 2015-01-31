725c882fa95ec9823a71b2becb3396d97842e61b
krujos <jkruck@pivotal.io>
2015-01-31 09:22:30 -0600

    Initial Commit

---
+++ src/main/java/distsched/DistributedSchedulerApplication.java
@@ -0,0 +1,12 @@
+package distsched;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedSchedulerApplication.class, args);
+    }
+}
